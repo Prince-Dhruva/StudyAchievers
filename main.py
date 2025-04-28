70a8499feacf255e7082f4223ca8a83fed55a113
Prince Khatri <princekhatri1013@gmail.com>
2025-04-28 09:49:23 +0530

    Refactor main function and improve data extraction logic with enhanced print statements

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def extractData(ssName,row1,row2,col1,col2):
-    
+    # For different Group or sheets we are extracting data
     for i,ss in enumerate(ssName):
         if i==0:
             row1_data = ss.row_values(row1)[col1-1:col2]
@@ -18,31 +18,34 @@ def extractData(ssName,row1,row2,col1,col2):
     return row1_data,row2_data
 
 
-def main(ssName,sheet,rankListName,row1,row2,col1,col2):
+def main(ssName:list,sheet,rankListName:list,row1,row2:list,col1,col2):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     creds = ServiceAccountCredentials.from_json_keyfile_name(os.getenv("CREDENTIALS_FILE"),scope)
     client = gspread.authorize(creds)
-    print("Authorisation completed")
-    print(f"opening Sheet:{ssName[0]}:",end="")
+    print("Authorisation completed",flush=True)
+    print(f"opening Sheet:{ssName[0]}:",end="",flush=True)
     sheet1 = client.open(ssName[0]).worksheet(sheet)
-    print("SUcessfull")
+    print("SUcessfull",flush=True)
     print(f"opening Sheet:{ssName[1]}:",end="")
     sheet2 = client.open(ssName[1]).worksheet(sheet)
-    print("SUcessfull")
+    print("SUcessfull",flush=True)
+    message="X-----X-----X-----X-----X\n"
+    for i,ss in enumerate(ssName):
+        row1_data,row2_data = extractData([sheet1,sheet2],row1,row2[i],col1,col2)
 
-    row1_data,row2_data = extractData([sheet1,sheet2],row1,row2,col1,col2)
-
-    print('Data Extracted')
-    data_dict = {k:float(v) for k,v in zip(row1_data,row2_data) if k and v}
-    sorted_items = sorted(data_dict.items(),key=lambda x: x[1])
-    sorted_items = sorted_items[::-1]
-    print("Rank list generated")
-    message = f"{rankListName} of this week:\n"
-    i=0
-    for key,value in sorted_items:
-        message += f"{i+1}: {key} scored {value}\n"
-        i+=1
-    print("Message written")
+        print('Data Extracted',flush=True)
+        data_dict = {k:float(v) for k,v in zip(row1_data,row2_data) if k and v}
+        sorted_items = sorted(data_dict.items(),key=lambda x: x[1])
+        sorted_items = sorted_items[::-1]
+        print("Rank list generated",flush=True)
+        message += f"{rankListName[i]} of this week:\n"
+        i=0
+        for key,value in sorted_items:
+            message += f"{i+1}: {key} scored {value}\n"
+            i+=1
+        print("Message written",flush=True)
+        message+="-----X-----X-----X-----X"
+        print(message,flush=True)
     account_sid = os.getenv("TWILIO_SID")
     auth_token = os.getenv("TWILIO_TOKEN")
     from_number = os.getenv("TWILIO_FROM")
@@ -51,15 +54,16 @@ def main(ssName,sheet,rankListName,row1,row2,col1,col2):
 
     client = Client(account_sid,auth_token)
     client.messages.create(body=message,from_=from_number,to=to_number)
-    print("Message sent")
+    print("Message sent",flush=True)
 
 if __name__ == '__main__':
     print("Start")
     ssName1 = os.getenv("SS_NAME_1")
     ssName2 = os.getenv("SS_NAME_2")
     sheet = os.getenv("SHEET")
-    print("Env variables Opened:",[ssName1,ssName2],sheet)
-    main([ssName1,ssName2],sheet,"Study Acheivers",25,32,3,8)
-    main([ssName1,ssName2],sheet,"SP Book Reading Acheivers",25,30,3,8)
+    print("Env variables Opened:",[ssName1,ssName2],sheet,flush=True)
+
+    main([ssName1,ssName2],sheet,["Study Acheivers","SP Book Reading Acheivers"],25,[32,30],3,8)
+    # main([ssName1,ssName2],sheet,"SP Book Reading Acheivers",25,30,3,8)
 
 
